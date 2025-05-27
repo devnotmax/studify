5c527d8cf42d48a21d8b863da5d10c04b1b025f6
devnotmax <develop.maxsj@gmail.com>
2025-05-27 18:13:59 -0300

    enrutado

---
+++ src/theme.ts
@@ -0,0 +1,27 @@
+// src/theme.ts
+const lightTheme = {
+  name: "light",
+  colors: {
+    primaryDark: "#2B4E52",
+    greenPastel: "#D6E5C6",
+    peach: "#F6DFD5",
+    brown: "#BAA398",
+    lavender: "#EDECE8",
+    lightBorder: "#bcb7cc",
+    white: "#ffffff",
+    lightGray: "#e8e8e3",
+    periwinkle: "#aeb7d6",
+    mossGreen: "#8aa18e",
+    mustard: "#D3B87F",
+    taupe: "#aba29a",
+    sand: "#d6d0c4",
+    offWhite: "#e7e2d8",
+    burgundy: "#8b5a5a",
+    iceBlue: "#c9d5dc",
+    teal: "#55686c",
+    background: "#f4f1f6",
+    text: "#565656",
+  },
+};
+
+export default lightTheme;
