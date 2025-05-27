469b74ef38e8ff812047620999f3f98b039de0a7
devnotmax <develop.maxsj@gmail.com>
2025-05-27 17:55:09 -0300

    heart icon

---
+++ src/icons/HeartIcon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import type { SVGProps } from "react";
+
+export function HeartIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M19.071 13.142L13.414 18.8a2 2 0 0 1-2.828 0l-5.657-5.657A5 5 0 1 1 12 6.072a5 5 0 0 1 7.071 7.07"
+      ></path>
+    </svg>
+  );
+}
